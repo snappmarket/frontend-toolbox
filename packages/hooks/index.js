7dde36ed4b2e237a645dad4ff30d723481c66131
JafarRezaei <jafar.rezaei.ard@gmail.com>
2020-01-24 07:07:34 -0800

    fix importing typo mistake

--- packages/hooks/index.js
+++ packages/hooks/index.js
@@ -1,7 +1,7 @@
-export { default as useDebounce } from './packages/useDebounce';
-export { default as useDidUpdateEffect } from './packages/useDidUpdateEffect';
-export { default as useFocus } from './packages/useFocus';
-export { default as useForceUpdate } from './packages/useForceUpdate';
-export { default as useGeoLocation } from './packages/useGeoLocation';
-export { default as useRouteChange } from './packages/useRouteChange';
-export { default as useTimer } from './packages/useTimer';
+export { default as useDebounce } from './packages/useDebounce/index';
+export { default as useDidUpdateEffect } from './packages/useDidUpdateEffect/index';
+export { default as useFocus } from './packages/useFocus/index';
+export { default as useForceUpdate } from './packages/useForceUpdate/index';
+export { default as useGeoLocation } from './packages/useGeoLocation/index';
+export { default as useRouteChange } from './packages/useRouteChange/index';
+export { default as useTimer } from './packages/useTimer/index';
