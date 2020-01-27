df8205ba66d5ea66963ee2bbe07bd7656fc8b574
JafarRezaei <jafar.rezaei.ard@gmail.com>
2020-01-27 01:40:53 -0800

    fix container bug with rest parameters

--- packages/ui/packages/GridSystem/src/Container.js
+++ packages/ui/packages/GridSystem/src/Container.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import { StyledContainer } from './styles';
 
 const Container = (props) => {
-  const { className, children } = props;
+  const { className, children, ...rest } = props;
 
-  return <StyledContainer className={className}>{children}</StyledContainer>;
+  return <StyledContainer className={className} {...rest}>{children}</StyledContainer>;
 };
 
 Container.propTypes = {
