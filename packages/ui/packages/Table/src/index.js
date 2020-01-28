497d3e06ab674821362082bd04ccff2b82192e21
Reza Erami <reer217@gmail.com>
2020-01-28 19:22:44 +0330

    add header to table

--- packages/ui/packages/Table/src/index.js
+++ packages/ui/packages/Table/src/index.js
@@ -2,22 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
   StyledTable,
+  StyledTableHeader,
+  StyledTableRow,
+  StyledTableHeaderColumn,
 } from './style';
 
+/* eslint-disable react/no-array-index-key */
 const Table = props => {
-  const { className } = props;
+  const { headers, className } = props;
   return (
     <StyledTable className={className}>
+      {!!headers.length && (
+        <StyledTableHeader>
+          <StyledTableRow>
+            {headers.map((item, key) => (
+              <StyledTableHeaderColumn key={key}>
+                {item}
+              </StyledTableHeaderColumn>
+            ))}
+          </StyledTableRow>
+        </StyledTableHeader>
+      )}
     </StyledTable>
   );
 };
 
 Table.propTypes = {
   className: PropTypes.string,
+  headers: PropTypes.array,
 };
 
 Table.defaultProps = {
   className: '',
+  headers: [],
 };
 
 export default Table;
--- packages/ui/packages/Table/src/style/index.js
+++ packages/ui/packages/Table/src/style/index.js
@@ -1,7 +1,13 @@
 import Styled from 'styled-components';
 
 const StyledTable = Styled.table``;
+const StyledTableHeader = Styled.thead``;
+const StyledTableRow = Styled.tr``;
+const StyledTableHeaderColumn = Styled.th``;
 
 export {
   StyledTable,
+  StyledTableHeader,
+  StyledTableRow,
+  StyledTableHeaderColumn,
 };
