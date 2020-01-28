998d9766dead594a9ca23679dbf79e30a04d5d17
Reza Erami <reer217@gmail.com>
2020-01-28 19:48:03 +0330

    change background color of header and footer

--- packages/ui/packages/Table/src/style/index.js
+++ packages/ui/packages/Table/src/style/index.js
@@ -5,9 +5,14 @@ const StyledTable = Styled.table`
   border-radius: calc(${(props) => props.theme.defaultRem} * 0.5);
 `;
 
-const StyledTableHeader = Styled.thead``;
+const StyledTableHeader = Styled.thead`
+  background-color: ${(props) => props.theme.colors.gray.bright};
+`;
 const StyledTableBody = Styled.tbody``;
-const StyledTableFooter = Styled.tfoot``;
+const StyledTableFooter = Styled.tfoot`
+  background-color: ${(props) => props.theme.colors.gray.bright};
+`;
+
 const StyledTableRow = Styled.tr``;
 const StyledTableHeaderColumn = Styled.th``;
 const StyledTableColumn = Styled.td``;
