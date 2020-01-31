52027157af1f0dfcf2eb631e613a9c35c9ec13a9
Reza <Erami>
2020-01-31 13:30:34 +0330

    test for delete cookie

--- packages/helpers/packages/CookieHelpers/test/index.spec.js
+++ packages/helpers/packages/CookieHelpers/test/index.spec.js
@@ -1,9 +1,9 @@
 import * as CookieHelpers from '../index'
 
+beforeAll(() => {
+  document.cookie="foo=bar";
+});
 describe('CookieHelpers', () => {
-  beforeEach(() => {
-    document.cookie="foo=bar";
-  });
   describe('getCookie', () => {
     it('should return value of a cookie', () => {
       expect(CookieHelpers.getCookie('foo')).toEqual('bar');
@@ -15,7 +15,14 @@ describe('CookieHelpers', () => {
   describe('setCookie', () => {
     it('should set a cookie in document', () => {
       CookieHelpers.setCookie('bar', 'foo');
-      expect(document.cookie).toEqual('foo=bar; bar=foo')
+      expect(document.cookie).toEqual('foo=bar; bar=foo');
+      CookieHelpers.deleteCookie('bar'); // remove it for next test
+    });
+  });
+  describe('deleteCookie', () => {
+    it('should delete a cookie in document', () => {
+      CookieHelpers.deleteCookie('foo');
+      expect(document.cookie).toEqual('');
     });
   });
 });
