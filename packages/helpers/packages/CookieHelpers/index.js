cdf52a6f3b48b4e9146c21b8a1c2acf90ffb437d
Reza <Erami>
2020-01-31 12:42:37 +0330

    fixed the expires date

--- packages/helpers/packages/CookieHelpers/index.js
+++ packages/helpers/packages/CookieHelpers/index.js
@@ -39,6 +39,6 @@ export const setCookie = (name, value, expireDays = 90, path = '/') => {
  * @param name
  */
 export const deleteCookie = (name) => {
-  const expires = `expires=${0}`;
+  const expires = `expires=Thu, 01 Jan 1970 00:00:01 GMT`;
   document.cookie = `${name}=${null};${expires}`;
 };
