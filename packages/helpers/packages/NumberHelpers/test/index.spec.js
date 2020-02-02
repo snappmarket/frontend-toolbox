00323d6d3de5c593edc379bccf62f614f626c00b
Reza Erami <reer217@gmail.com>
2020-02-02 16:53:03 +0330

    test for cellphone validation regex

--- packages/helpers/packages/NumberHelpers/test/index.spec.js
+++ packages/helpers/packages/NumberHelpers/test/index.spec.js
@@ -8,11 +8,21 @@ describe('NumberHelpers', () => {
       expect(NumberHelpers.persianNumber(payload)).toEqual(expected);
     });
   });
-  describe('english', () => {
-    it('should convert english number to persian', () => {
+  describe('englishNumber', () => {
+    it('should convert persian number to english', () => {
       const payload = '۱۲۳';
       const expected = '123';
       expect(NumberHelpers.englishNumber(payload)).toEqual(expected);
+    });
+  });
+  describe('cellphoneValidate', () => {
+    it('should pass the cellphone pattern', () => {
+      const cellphone = '09121234567';
+      expect(!!cellphone.match(NumberHelpers.cellphoneValidate())).toEqual(true);
+    });
+    it('should not pass the cellphone pattern', () => {
+      const cellphone = '0123456';
+      expect(!!cellphone.match(NumberHelpers.cellphoneValidate())).toEqual(false);
     });
   });
 });
