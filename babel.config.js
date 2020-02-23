c12672910ebfcfd6adeeef50e09730b9de32ab2d
JafarRezaei <jafar.rezaei.ard@gmail.com>
2020-02-23 05:39:41 -0800

    big improvement on tree shakable hooks build

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,102 @@
-module.exports = {
-  presets: [
+// module.exports = {
+//   presets: [
+//     [
+//       '@babel/preset-env',
+//       {
+//         modules: false,
+//       },
+//     ],
+//     '@babel/preset-react',
+//   ],
+//   ignore: [/@babel[\\|/]runtime/],
+//   plugins: [
+//     'babel-plugin-transform-react-constant-elements',
+//     'babel-plugin-transform-dev-warning',
+//     [
+//       'babel-plugin-transform-react-remove-prop-types',
+//       {
+//         mode: 'unsafe-wrap',
+//       },
+//     ],
+//     'babel-plugin-optimize-clsx',
+//     ['@babel/plugin-proposal-class-properties', { loose: true }],
+//     ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+//     // for IE 11 support
+//     '@babel/plugin-transform-object-assign',
+//     ['@babel/plugin-transform-runtime'],
+//   ],
+// };
+
+
+let defaultPresets;
+
+// We release a ES version of Material-UI.
+// It's something that matches the latest official supported features of JavaScript.
+// Nothing more (stage-1, etc), nothing less (require, etc).
+if (process.env.BABEL_ENV === 'es') {
+  defaultPresets = [];
+} else {
+  defaultPresets = [
     [
       '@babel/preset-env',
       {
-        modules: false,
+        modules: ['esm', 'production-umd'].includes(process.env.BABEL_ENV) ? false : 'commonjs',
       },
     ],
-    '@babel/preset-react',
+  ];
+}
+
+const productionPlugins = [
+  'babel-plugin-transform-react-constant-elements',
+  'babel-plugin-transform-dev-warning',
+  ['babel-plugin-react-remove-properties', { properties: ['data-mui-test'] }],
+  [
+    'babel-plugin-transform-react-remove-prop-types',
+    {
+      mode: 'unsafe-wrap',
+    },
   ],
-  ignore: ['node_modules/**', /@babel[\\|/]runtime/],
+];
+
+module.exports = {
+  presets: defaultPresets.concat(['@babel/preset-react']),
   plugins: [
-    ['babel-plugin-styled-components'],
+    'babel-plugin-optimize-clsx',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
-    ['@babel/plugin-transform-runtime', { useESModules: true }],
+    // any package needs to declare 7.4.4 as a runtime dependency. default is ^7.0.0
+    ['@babel/plugin-transform-runtime', { version: '^7.4.4' }],
+    // for IE 11 support
+    '@babel/plugin-transform-object-assign',
   ],
+  ignore: [/@babel[\\|/]runtime/], // Fix a Windows issue.
+  env: {
+    cjs: {
+      plugins: productionPlugins,
+    },
+    development: {
+      plugins: [
+        [
+          'babel-plugin-module-resolver',
+          {
+            alias: {
+              modules: './modules',
+            },
+          },
+        ],
+      ],
+    },
+    esm: {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+    es: {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+    production: {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+    'production-umd': {
+      plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+  },
 };
--- packages/hooks/index.js
+++
@@ -1,7 +0,0 @@
-export { default as useDebounce } from './packages/useDebounce/index';
-export { default as useDidUpdateEffect } from './packages/useDidUpdateEffect/index';
-export { default as useFocus } from './packages/useFocus/index';
-export { default as useForceUpdate } from './packages/useForceUpdate/index';
-export { default as useGeoLocation } from './packages/useGeoLocation/index';
-export { default as useRouteChange } from './packages/useRouteChange/index';
-export { default as useTimer } from './packages/useTimer/index';
