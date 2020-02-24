44fd39c4cd59229cdbe172652e47b9e694314acb
JafarRezaei <jafar.rezaei.ard@gmail.com>
2020-02-24 04:32:47 -0800

    improve hooks

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,3 @@
-// module.exports = {
-//   presets: [
-//     [
-//       '@babel/preset-env',
-//       {
-//         modules: false,
-//       },
-//     ],
-//     '@babel/preset-react',
-//   ],
-//   ignore: [/@babel[\\|/]runtime/],
-//   plugins: [
-//     'babel-plugin-transform-react-constant-elements',
-//     'babel-plugin-transform-dev-warning',
-//     [
-//       'babel-plugin-transform-react-remove-prop-types',
-//       {
-//         mode: 'unsafe-wrap',
-//       },
-//     ],
-//     'babel-plugin-optimize-clsx',
-//     ['@babel/plugin-proposal-class-properties', { loose: true }],
-//     ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
-//     // for IE 11 support
-//     '@babel/plugin-transform-object-assign',
-//     ['@babel/plugin-transform-runtime'],
-//   ],
-// };
-
 let defaultPresets;
 
 // We release a ES version of Material-UI.
@@ -74,6 +45,9 @@ module.exports = {
     /@babel[\\|/]runtime/,
     'node_modules/**',
     '**/dist/**',
+    '**/LICENCE',
+    '**/README.md',
+    '**/package-lock.json',
     '**/*.spec.js',
     '**/rollup.config.js',
   ],
