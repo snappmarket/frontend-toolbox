04e4151c1e4feda5f991de7a7edd059dd83bfc6b
mostafa rastegar <mostafa.rastegar@snapp.market>
2020-03-10 13:02:50 +0330

    Refactor folder

--- packages/ui/packages/Carousel/src/index.js
+++ packages/ui/packages/Carousel/src/index.js
@@ -1,1 +1,37 @@
-export { default as Slider } from './core';
+import * as React from 'react';
+import { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { Slider } from './core/index';
+import { StyledSimpleCarousel } from './core/styles';
+
+const SimpleCarousel = (props) => {
+  const { className, children, slideConfig } = props;
+  const sliderRed = useRef(null);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-new
+    new Slider({
+      slider: sliderRed.current,
+      ...slideConfig,
+    });
+  }, []);
+
+  return (
+    <StyledSimpleCarousel>
+      <div className={`slider ${className}`} ref={sliderRed}>
+        <div className="wrapper">
+          <div className="slides">{children}</div>
+        </div>
+      </div>
+    </StyledSimpleCarousel>
+  );
+};
+SimpleCarousel.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  slideConfig: PropTypes.object.isRequired,
+};
+SimpleCarousel.defaultProps = {
+  className: '',
+};
+export default SimpleCarousel;
