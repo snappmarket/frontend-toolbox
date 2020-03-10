c49243d612b6933bcbcea347dce9d80fd52aad58
jafarRezaei <bomber.man87@yahoo.com>
2020-03-10 09:26:29 +0330

    lint and code style improved

--- packages/ui/packages/Carousel/index.js
+++ packages/ui/packages/Carousel/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'; 
+import * as React from 'react';
 import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Slider } from './src/index';
@@ -6,25 +6,26 @@ import { StyledSimpleCarousel } from './src/styles';
 
 const SimpleCarousel = (props) => {
   const { className, children, slideConfig } = props;
-  const refrence = useRef();
+  const sliderRed = useRef(null);
+
   useEffect(() => {
     // eslint-disable-next-line no-new
     new Slider({
-      slider: refrence.current,
+      slider: sliderRed.current,
       ...slideConfig,
     });
   }, []);
 
   return (
     <StyledSimpleCarousel>
-      <div className={`slider ${className}`} ref={refrence}>
+      <div className={`slider ${className}`} ref={sliderRed}>
         <div className="wrapper">
           <div className="slides">{children}</div>
         </div>
       </div>
     </StyledSimpleCarousel>
   );
-}
+};
 SimpleCarousel.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
