471eee35cf18e72b590c1c9e735fc691a05a7372
Reza <Erami>
2020-03-12 14:00:03 +0330

    used dangerouslySetInnerHTML instead of children for putting schema in jsonLd script because it's not SSR friendly, and ssr turns qoutation marks into qoutation unicode, so it will make SSR result not equal to CST result and that cuases rehydration which makes the page be re-rendered

--- packages/ui/packages/JsonLD/src/index.js
+++ packages/ui/packages/JsonLD/src/index.js
@@ -2,9 +2,10 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 
 const JsonLD = ({ schema }) => (
-  <script type="application/ld+json">
-    {JSON.stringify(schema)}
-  </script>
+  <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+  />
 );
 
 JsonLD.propTypes = {
