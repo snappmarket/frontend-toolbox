d8c83f40c5e142ea71a1e6dfc6d2a8549925d5c2
Reza <Erami>
2020-03-14 20:41:02 +0330

    test for universall call with posit method

--- packages/helpers/packages/HttpHelpers/test/index.spec.js
+++ packages/helpers/packages/HttpHelpers/test/index.spec.js
@@ -1,10 +1,11 @@
 import fetch from 'isomorphic-unfetch';
 import * as HttpHelpers from '../index';
-jest.mock('isomorphic-unfetch');
 
+jest.mock('isomorphic-unfetch');
 afterEach(() => {
   jest.clearAllMocks();
 });
+
 describe('HttpHelpers', () => {
   describe('fetchWithTimeOut', () => {
     it('should fetch, and fetch should win the race', () => { // @todo: should write another test when fetch loses the race and times out
@@ -17,6 +18,39 @@ describe('HttpHelpers', () => {
   });
   describe('universalCall', () => {
     it('should call an API using universall fetch', () => {
+      const url = 'http://snapp.market';
+      const params = { foo: 'bar' };
+      const options = {
+        credentials: 'include',
+        headers: {},
+        method: 'GET',
+      };
+      HttpHelpers.universalCall({
+        url, params, ...options,
+      });
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenCalledWith(`${url}?foo=bar`, options);
+    });
+
+    it('should call an API using universall fetch with jwtToken', () => {
+      const url = 'http://snapp.market';
+      const data = { foo: 'bar' };
+      const options = { credentials: 'include', headers: {}, method: 'POST' };
+      const jwtToken = 'here is my token';
+      HttpHelpers.universalCall({
+        url, data, jwtToken, ...options,
+      });
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenCalledWith(`${url}`, {
+        ...options,
+        body: JSON.stringify(data),
+        headers: {
+          authorization: `Bearer ${jwtToken}`,
+        },
+      });
+    });
+
+    it('should call an API using universall fetch and get response', () => {
       const url = 'http://snapp.market';
       const params = { foo: 'bar' };
       const options = { credentials: 'include', headers: {}, method: 'GET' };
