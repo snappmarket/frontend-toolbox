9628c4a58488fe401cc0a9f02dad5c5a2ec36fb8
Reza <Erami>
2020-03-14 22:54:53 +0330

    increased coverage of httpHelper tests

--- packages/helpers/packages/HttpHelpers/test/index.spec.js
+++ packages/helpers/packages/HttpHelpers/test/index.spec.js
@@ -2,22 +2,37 @@ import fetch from 'isomorphic-unfetch';
 import * as HttpHelpers from '../index';
 
 jest.mock('isomorphic-unfetch');
+const mockResponse = { foo: 'bar' };
+const mockHeaders = {
+  'content-type': 'application/json',
+};
+beforeEach(() => {
+  fetch.mockReturnValue(Promise.resolve({
+    json: () => (mockResponse),
+    headers: {
+      get: (key) => mockHeaders[key],
+    },
+    ok: true,
+  }));
+});
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 describe('HttpHelpers', () => {
   describe('fetchWithTimeOut', () => {
-    it('should fetch, and fetch should win the race', () => { // @todo: should write another test when fetch loses the race and times out
+    it('should fetch, and fetch should win the race', async () => { // @todo: should write another test when fetch loses the race and times out
       const url = 'http://snapp.market';
       const options = { foo: 'bar' };
-      HttpHelpers.fetchWithTimeOut(url, options);
+      const response = await HttpHelpers.fetchWithTimeOut(url, options);
+      const result = await response.json();
       expect(fetch).toHaveBeenCalledTimes(1);
       expect(fetch).toHaveBeenCalledWith(url, options);
+      expect(result).toEqual(mockResponse);
     });
   });
   describe('universalCall', () => {
-    it('should call an API using universall fetch', () => {
+    it('should call an API using universal fetch', async () => {
       const url = 'http://snapp.market';
       const params = { foo: 'bar' };
       const options = {
@@ -25,19 +40,18 @@ describe('HttpHelpers', () => {
         headers: {},
         method: 'GET',
       };
-      HttpHelpers.universalCall({
-        url, params, ...options,
-      });
+      const response = await HttpHelpers.universalCall({ url, params, ...options });
       expect(fetch).toHaveBeenCalledTimes(1);
       expect(fetch).toHaveBeenCalledWith(`${url}?foo=bar`, options);
+      expect(response).toEqual(mockResponse);
     });
 
-    it('should call an API using universall fetch with jwtToken', () => {
+    it('should call an API using universal fetch with jwtToken', async () => {
       const url = 'http://snapp.market';
       const data = { foo: 'bar' };
       const options = { credentials: 'include', headers: {}, method: 'POST' };
       const jwtToken = 'here is my token';
-      HttpHelpers.universalCall({
+      const response = await HttpHelpers.universalCall({
         url, data, jwtToken, ...options,
       });
       expect(fetch).toHaveBeenCalledTimes(1);
@@ -48,15 +62,17 @@ describe('HttpHelpers', () => {
           authorization: `Bearer ${jwtToken}`,
         },
       });
+      expect(response).toEqual(mockResponse);
     });
 
-    it('should call an API using universall fetch and get response', () => {
+    it('should call an API using universal fetch and get response', async () => {
       const url = 'http://snapp.market';
       const params = { foo: 'bar' };
       const options = { credentials: 'include', headers: {}, method: 'GET' };
-      HttpHelpers.universalCall({ url, params, ...options });
+      const response = await HttpHelpers.universalCall({ url, params, ...options });
       expect(fetch).toHaveBeenCalledTimes(1);
       expect(fetch).toHaveBeenCalledWith(`${url}?foo=bar`, options);
+      expect(response).toEqual(mockResponse);
     });
   });
 });
