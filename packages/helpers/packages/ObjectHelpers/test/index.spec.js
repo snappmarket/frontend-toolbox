ce07c6ee48930070d5b13d96cd6f88e27ac1fd9e
Reza <Erami>
2020-03-15 19:34:35 +0330

    better coverage for removeBykey

--- packages/helpers/packages/ObjectHelpers/test/index.spec.js
+++ packages/helpers/packages/ObjectHelpers/test/index.spec.js
@@ -3,12 +3,12 @@ import * as ObjectHelpers from '../index';
 describe('ObjectHelpers', () => {
   describe('removeByKey', () => {
     it('should remove a non-numeric key from an object', () => {
-      const payload = { foo: 'bar' };
-      expect(ObjectHelpers.removeByKey(payload, 'foo')).toEqual({});
+      const payload = { foo: 'bar', bar: 'bar' };
+      expect(ObjectHelpers.removeByKey(payload, 'foo')).toEqual({ bar: 'bar' });
     });
     it('should remove a numeric key from an object', () => {
-      const payload = { 0: 'bar' };
-      expect(ObjectHelpers.removeByKey(payload, 0)).toEqual({ 0: 'bar' });
+      const payload = { 0: 'foo', 1: 'bar' };
+      expect(ObjectHelpers.removeByKey(payload, 0)).toEqual({ 1: 'bar' });
     });
   });
 });
