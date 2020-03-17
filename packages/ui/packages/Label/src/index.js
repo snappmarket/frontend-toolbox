9706e1391df0c61f3e117ab7f643b0b11743b052
mahsa <mahsa_mesbah@yahoo.com>
2020-03-17 16:46:46 +0330

    add testCase for label component

--- packages/ui/packages/Label/src/index.js
+++ packages/ui/packages/Label/src/index.js
@@ -11,10 +11,10 @@ const Label = (props) => {
   const { type, children, ...rest } = props;
 
   const labels = {
-    primary: <StyledPrimaryLabel {...rest}>{children}</StyledPrimaryLabel>,
-    material: <StyledMaterialLabel {...rest}>{children}</StyledMaterialLabel>,
-    outline: <StyledOutlineLabel {...rest}>{children}</StyledOutlineLabel>,
-    quiet: <StyledQuietLabel {...rest}>{children}</StyledQuietLabel>,
+    primary: <StyledPrimaryLabel data-testid="label" {...rest}>{children}</StyledPrimaryLabel>,
+    material: <StyledMaterialLabel data-testid="label" {...rest}>{children}</StyledMaterialLabel>,
+    outline: <StyledOutlineLabel data-testid="label" {...rest}>{children}</StyledOutlineLabel>,
+    quiet: <StyledQuietLabel data-testid="label" {...rest}>{children}</StyledQuietLabel>,
   };
 
   return labels[type];
