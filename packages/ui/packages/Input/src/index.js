12f9f1a843098a4566528158aaae364cda1af035
mahsa <mahsa_mesbah@yahoo.com>
2020-03-17 15:53:34 +0330

    add testCase for Input component

--- packages/ui/packages/Input/src/index.js
+++ packages/ui/packages/Input/src/index.js
@@ -13,15 +13,16 @@ const Input = React.forwardRef((props, ref) => {
     status, message, label, id, required, className, ...rest
   } = props;
   return (
-    <StyledInputWrapper>
+    <StyledInputWrapper data-testid="inputWrapper">
       {label && props.id && (
-        <StyledLabel htmlFor={id}>
+        <StyledLabel data-testid="inputLabel" htmlFor={id}>
           {label}
           {' '}
-          {required && <StyledStar>*</StyledStar>}
+          {required && <StyledStar data-testid="labelRequiredFlag">*</StyledStar>}
         </StyledLabel>
       )}
       <StyledInput
+        data-testid="input"
         className={className}
         status={status}
         ref={ref}
@@ -29,7 +30,7 @@ const Input = React.forwardRef((props, ref) => {
         {...rest}
       />
       {Object.keys(message).length > 0 && (
-        <StyledMessage type={message.type}>{message.content}</StyledMessage>
+        <StyledMessage data-testid="inputMessages" type={message.type}>{message.content}</StyledMessage>
       )}
     </StyledInputWrapper>
   );
