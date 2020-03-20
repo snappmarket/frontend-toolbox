c2d07253ffe02a9b74bcf9fc58b92e0c56493e8d
jafarRezaei <bomber.man87@yahoo.com>
2020-03-20 13:06:55 +0330

    improve readme and docz

---
+++ packages/icons/packages/Emoji/index.js
@@ -0,0 +1,5 @@
+export { default as DissatisfiedEmoji } from './DissatisfiedEmoji';
+export { default as HappyEmoji } from './HappyEmoji';
+export { default as LaughEmoji } from './LaughEmoji';
+export { default as NormalEmoji } from './NormalEmoji';
+export { default as SadEmoji } from './SadEmoji';
