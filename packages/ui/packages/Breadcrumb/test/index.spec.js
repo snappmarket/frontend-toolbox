0428bd501f69fa6a72604bfd654bfe7033680b6e
mostafa rastegar <m.rastegar991@gmail.com>
2020-03-24 14:04:38 +0430

    TestCase for BreadCrumb

--- packages/ui/packages/Breadcrumb/test/index.spec.js
+++ packages/ui/packages/Breadcrumb/test/index.spec.js
@@ -1,25 +1,65 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import { Wrapper, theme } from '../../../test/test.helpers';
+import { Wrapper } from '../../../test/test.helpers';
 import Breadcrumb from '../index';
-// import IconName from '../../../../icons/packages/FontIcons/InfoIcon';
 
-describe('Breadcrumb ui component tests', () => {
-  it('Should render with primary mode and blue color with normal shade and lg size at default', () => {
+describe('Should add class to element ', () => {
+  it('Should add class to element', () => {
     const { getByTestId } = render(
-      <Wrapper>
-        <Breadcrumb />
-      </Wrapper>,
+      <Wrapper><Breadcrumb className="my-custom-class" /></Wrapper>,
     );
+    expect(getByTestId('breadcrumb')).toHaveClass('my-custom-class');
+  });
+});
 
-    expect(getByTestId('button')).toHaveStyle({
-      backgroundColor: theme.colors.blue.normal,
-      color: theme.colors.white,
-      height: 'calc(4.5 * 1rem)',
-      fontSize: 'calc(1.8 * 1rem)',
-      minWidth: 'calc(20.6 * 1rem)',
-    });
+
+describe('Should handle items', () => {
+
+  const renderByContainer = data => {
+    const { container } = render(
+      <BrowserRouter>
+        <Wrapper><Breadcrumb items={data} /></Wrapper>
+      </BrowserRouter>,
+    );
+    return container;
+  };
+
+  const lengthOfAllSvg = data => {
+    const selectAllSvg = renderByContainer(data).querySelectorAll('svg');
+    return Object.keys(selectAllSvg).length;
+  };
+
+  it('Should create a link without svg icon for Breadcrumb items', () => {
+
+    const mockData = [
+      {
+        link: '/product-1',
+        title: 'product-1',
+      },
+    ];
+
+    expect(lengthOfAllSvg(mockData)).toBe(0);
+    expect(renderByContainer(mockData)).toContainHTML('<a href="/product-1">product-1</a>');
+  });
+
+  it('Should render 2 svg arrow icon for 3 items in Breadcrumb ', () => {
+    const mockData = [
+      {
+        link: '/product-1',
+        title: 'product-1',
+      },
+      {
+        link: '/product-2',
+        title: 'product-2',
+      },
+      {
+        link: '/product-3',
+        title: 'product-3',
+      },
+    ];
+    expect(lengthOfAllSvg(mockData)).toBe(2);
   });
 });
