ca093bdfe977806cfac233efcb030b6a4fa1800c
Reza <Erami>
2020-03-27 17:32:25 +0430

    concat readme and jsdoc result to make better mdx file

--- scripts/docs/jsdocs-to-docz.js
+++ scripts/docs/jsdocs-to-docz.js
@@ -15,12 +15,13 @@ async function run(){
 
       const fileName = file.substr(file.lastIndexOf('/')+1);
       const mdxFileName = fileName.replace(/\.(.*)/,'.mdx');
-
+      const markdownFile = `${fileParentDir}/README.md`;
       try {
+        const markdownContent = fs.existsSync(markdownFile) ? fs.readFileSync(markdownFile, 'utf-8') : '';
         exec(`jsdoc2md ${file}`, (_, stdout)=>{
           const output = stdout.replace(/<(|\/)code>/g, '');
-          const doczInfo = `---\nname: ${packagesName}\nroute: /${packageRoute}\nmenu: ${capitalizedParent}\n---\n## ${packagesName}`;
-          const content = `${doczInfo}\n${output}`;
+          const doczInfo = `---\nname: ${packagesName}\nroute: /${packageRoute}\nmenu: ${capitalizedParent}\n---\n`;
+          const content = `${doczInfo}\n\n${markdownContent}\n\n${output}`;
 
           console.log(`writing docz for ${file}`);
           fs.writeFile(`${fileParentDir}/${mdxFileName}`, content, (error) => {
