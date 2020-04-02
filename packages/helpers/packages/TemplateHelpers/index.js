2f8c930a9209e4e893103a094fb0480c25e2758c
Reza <Erami>
2020-04-02 14:37:52 +0430

    Fix JsDocs fix jsDocs that missed function tag fix jsDocs of template helpers fixed hexToRgb helper, no need to put returned value in variable modify the make shadow helpers to nurmal funcion cause it doesn't need to be IIFE

--- packages/helpers/packages/TemplateHelpers/index.js
+++ packages/helpers/packages/TemplateHelpers/index.js
@@ -1,66 +1,74 @@
-const DEFAULT_REM = '1rem';
+import theme from '../../../ui/packages/Theme/src/theme'
 
 /**
- * Convert hex to rgba
+ * @function
+ * @name hexToRgb
+ * @description Convert hex color to rgb color
  * @param hex
  * @returns {{red: number, green: number, blue: number}}
- * @constructor
  */
-export const HexToRgb = hex => {
+export const hexToRgb = hex => {
   if (!hex) {
     return { red: 0, green: 0, blue: 0 };
   }
+
   const hexColor = hex.replace('#', '');
-
-  const rgb = {};
-  rgb.red = parseInt(hexColor.substr(0, 2), 16);
-  rgb.green = parseInt(hexColor.substr(2, 2), 16);
-  rgb.blue = parseInt(hexColor.substr(4, 2), 16);
-
-  return rgb;
+  return {
+    red: parseInt(hexColor.substr(0, 2), 16),
+    green: parseInt(hexColor.substr(2, 2), 16),
+    blue: parseInt(hexColor.substr(4, 2), 16),
+  }
 };
 
 /**
- * Decide between darkness and lightness
- * @param color
+ * @function
+ * @name defineForegroundColor
+ * @description Decide between dark or light color based on background color contrast
+ * @param backgroundColor   {string}    background color in hex
  * @returns {string}
  */
-export const defineForegroundColor = color => {
-  const rgb = HexToRgb(color);
+export const defineForegroundColor = backgroundColor => {
+  const rgb = hexToRgb(backgroundColor);
   const average = (rgb.red * 299 + rgb.green * 587 + rgb.blue * 114) / 1000;
   return average > 128 ? 'taupe' : 'white';
 };
 
 /**
- * Make an rgba color from a hex
+ * @function
+ * @name makeRgba
+ * @description Make an rgba color suitable for CSS from a hex color
  * @param opacity
  * @param color
  * @returns {string}
  */
 export const makeRgba = (opacity, color) => {
-  const rgb = HexToRgb(color);
+  const rgb = hexToRgb(color);
   return `rgba(${rgb.red},${rgb.green},${rgb.blue}, ${opacity})`;
 };
 
 /**
- * Decide between rem and percent values
- * @param measurement
+ * @function
+ * @name decideMeasurement
+ * @description Decide
+ * @param measurement what measurement to use, given measurement or multiplying it in rem unit
  * @returns {string}
  */
 export const decideMeasurement = measurement =>
   typeof measurement === 'string'
     ? measurement
-    : `calc(${DEFAULT_REM} * ${measurement})`;
+    : `calc(${theme.defaultRem} * ${measurement})`;
 
 /**
- * Will create shadow in easy way
- * @param hOffset
- * @param vOffset
- * @param blur
- * @param spread
- * @param color
- * @param inset
- * @returns {function(*): string}
+ * @function
+ * @name makeShadow
+ * @description returns a shadow suitable for css
+ * @param   hOffset   {number}    horizontal offset of shadow, wil be multiplied in rem
+ * @param   vOffset   {number}    vertical offset of shadow, wil be multiplied in rem
+ * @param   blur      {number}    blur of the shadow, wil be multiplied in rem
+ * @param   spread    {number}    spread of the shadow, wil be multiplied in rem
+ * @param   color     {string}    color of the shadow
+ * @param   inset     {boolean}    blur of the shadow, wil be multiplied in rem
+ * @returns {string}
  */
 export const makeShadow = (
   hOffset,
@@ -69,10 +77,10 @@ export const makeShadow = (
   spread,
   color,
   inset = false,
-) => () => `calc(${DEFAULT_REM} * ${hOffset})
-  calc(${DEFAULT_REM} * ${vOffset}) 
-  calc(${DEFAULT_REM} * ${blur}) 
-  calc(${DEFAULT_REM} * ${spread}) 
-  ${color} 
-  ${inset || ''}
+) => `calc(${theme.defaultRem} * ${hOffset})
+  calc(${theme.defaultRem} * ${vOffset})
+  calc(${theme.defaultRem} * ${blur})
+  calc(${theme.defaultRem} * ${spread})
+  ${color}
+  ${inset ? 'inset' : ''}
 `;
