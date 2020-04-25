bec5aa54442394fde666e0ed8d653394b7ff69d6
jafarRezaei <bomber.man87@yahoo.com>
2020-04-26 00:43:50 +0430

    fix jest static files port

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,10 @@ module.exports = {
     '!scripts',
     '!*.config.js',
   ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mock__/fileMock.js",
+    "\\.(css|less|scss|sass)$": "<rootDir>/test/__mock__/styleMock.js"
+  },
   rootDir: process.cwd(),
   roots: ['<rootDir>'],
   testPathIgnorePatterns: ['<rootDir>/.docz'],
