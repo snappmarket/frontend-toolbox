9754bd78e6d75f19ef62e4f1e18b7ee4bcb889f7
unknown <reza.erami@snapp.market>
2020-04-29 12:26:03 +0430

    remove doczrc.js
    doczrc.js would be automatically cloned into the docz directory from the
    root of the project, so there's no need to have it in docz sub directory
    also.

--- .docz/doczrc.js
+++
@@ -1,7 +0,0 @@
-// doczrc.js
-export default {
-  title: 'Snappmarket frontend toolbox',
-  files: '**/*.{markdown,mdx}',
-  port: '9002',
-  ignore: ['readme.md', 'changelog.md', 'code_of_conduct.md', 'contributing.md', 'license.md'],
-};
