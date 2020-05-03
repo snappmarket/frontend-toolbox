1186891204a18f5d9b308ba58acf0c1c78f1d2bb
unknown <reza.erami@snapp.market>
2020-05-03 14:09:03 +0430

    fix loading components
    classname should be added to components to give us the ability to use
    them as styled component
    fixed condition of logo animation cause when you don't set any else
    statement, a false string would be set as the classname of the
    component.
    fixed color proptypes of circle wave loading,
    added docz for components.

--- packages/ui/packages/Loading/src/CircleWaveAnimation.js
+++ packages/ui/packages/Loading/src/CircleWaveAnimation.js
@@ -5,11 +5,13 @@ import { StyledCircleWave } from './styles';
 const CircleWaveAnimation = props => <StyledCircleWave {...props} />;
 
 CircleWaveAnimation.propTypes = {
+  className: PropTypes.string,
   size: PropTypes.number,
-  color: PropTypes.string,
+  color: PropTypes.oneOf(['gray', 'blue', 'red', 'green', 'yellow', 'orange']),
 };
 
 CircleWaveAnimation.defaultProps = {
+  className: '',
   size: 1.4,
   color: 'green',
 };
