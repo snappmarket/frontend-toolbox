2dca5081e995f943f825d1768ae021018208f1b3
unknown <reza.erami@snapp.market>
2020-05-03 14:47:33 +0430

    fiix progress bar component
    added classname to be able to use it as a styled component also
    added default props cause if the percent prop is not required, it should
    have a default prop then.

--- packages/ui/packages/ProgressBar/src/index.js
+++ packages/ui/packages/ProgressBar/src/index.js
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types';
 import { StyledProgressBarWrapper, StyledProgressBar } from './styles';
 
 const ProgressBar = props => {
-  const { percent, ...rest } = props;
+  const { percent, className } = props;
   return (
-    <StyledProgressBarWrapper data-testid="progressBarWrapper"  {...rest}>
+    <StyledProgressBarWrapper data-testid="progressBarWrapper"  className={className}>
       <StyledProgressBar data-testid="progressBar" percent={percent} />
     </StyledProgressBarWrapper>
   );
 };
 
 ProgressBar.propTypes = {
+  className: PropTypes.string,
   percent: PropTypes.number,
 };
 
-ProgressBar.defaultProps = {};
+ProgressBar.defaultProps = {
+  className: '',
+  percent: 0,
+};
+
 export default ProgressBar;
