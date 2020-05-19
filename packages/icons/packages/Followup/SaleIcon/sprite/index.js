ae07fd03912f5e074e736ac6aa85ecff7fe46511
jafarRezaei <bomber.man87@yahoo.com>
2020-05-19 18:49:22 +0430

    0.2.13

---
+++ packages/icons/packages/Followup/SaleIcon/sprite/index.js
@@ -0,0 +1,46 @@
+/**
+ * THIS IS AN AUTO GENERATED SPRITE FILE, CHANGES WILL NOT APPLY
+ */
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { useOptions, GlobalConfig } from '@snappmarket/config';
+
+if (GlobalConfig.options.importSpriteSVG) {
+  // eslint-disable-next-line global-require
+  require('./SaleIcon.svg');
+}
+
+const SaleIcon = ({ className, size }) => {
+  const options = useOptions();
+
+  let importPrefix = options.useSpriteFile ? `/${options.spriteSvgName}` : '';
+  if (options.publicPath !== '/') {
+    importPrefix = `/${options.publicPath}${importPrefix}`;
+  }
+
+  return (
+    <svg
+      data-testid="SaleIcon"
+      viewBox="0 0 41.417 37.039"
+      className={className}
+      style={{
+        width: size * 10,
+        height: size * 10,
+      }}
+      fill="currentColor"
+    >
+      <use xlinkHref={`${importPrefix}#SaleIcon`} />
+    </svg>
+  );
+};
+
+SaleIcon.propTypes = {
+  className: PropTypes.string,
+  size: PropTypes.number,
+};
+
+SaleIcon.defaultProps = {
+  size: 1.5,
+};
+
+export default SaleIcon;
