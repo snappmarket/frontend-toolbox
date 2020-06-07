7706bd1f730d18f84bbabd2d8f3290c1ecc1ebe7
jafarRezaei <bomber.man87@yahoo.com>
2020-06-07 18:25:59 +0430

    fix iterate method and improve coverage

--- packages/icons/test/index.spec.js
+++ packages/icons/test/index.spec.js
@@ -4,27 +4,23 @@ import * as Icons from '../packages/index';
 import * as SpriteIcons from '../packages/sprite/index';
 
 // RENDER TEST
-describe('Icons render test : ', () => {
-  Object.keys(Icons).forEach(icon => {
-    it(`should render ${icon} without crashing`, () => {
-      const div = document.createElement('div');
-      const Icon = Icons[icon];
+describe.each(Object.keys(Icons))('Icons render test : ', icon => {
+  it(`should render ${icon} without crashing`, () => {
+    const div = document.createElement('div');
+    const Icon = Icons[icon];
 
-      ReactDOM.render(<Icon />, div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
+    ReactDOM.render(<Icon />, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
 });
 
 // RENDER TEST
-describe('Sprite Icons render test : ', () => {
-  Object.keys(Icons).forEach(icon => {
-    it(`should render ${icon} without crashing`, () => {
-      const div = document.createElement('div');
-      const Icon = Icons[icon];
+describe.each(Object.keys(SpriteIcons))('Sprite Icons render test : ', icon => {
+  it(`should render ${icon} without crashing`, () => {
+    const div = document.createElement('div');
+    const Icon = Icons[icon];
 
-      ReactDOM.render(<Icon />, div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
+    ReactDOM.render(<Icon />, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
 });
