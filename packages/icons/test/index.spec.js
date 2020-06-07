a32b8a61b6b54569d72e9b70d222523f1d7fb9bf
jafarRezaei <bomber.man87@yahoo.com>
2020-06-07 15:16:21 +0430

    improve icons test

--- packages/icons/test/index.spec.js
+++ packages/icons/test/index.spec.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Icons from '../packages/index';
+import * as SpriteIcons from '../packages/sprite/index';
 
 // RENDER TEST
 describe('Icons render test : ', () => {
+  Object.keys(Icons).forEach(icon => {
+    it(`should render ${icon} without crashing`, () => {
+      const div = document.createElement('div');
+      const Icon = Icons[icon];
+
+      ReactDOM.render(<Icon />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+  });
+});
+
+// RENDER TEST
+describe('Sprite Icons render test : ', () => {
   Object.keys(Icons).forEach(icon => {
     it(`should render ${icon} without crashing`, () => {
       const div = document.createElement('div');
