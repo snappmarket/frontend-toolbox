cca0b0ce00ed958b22c46f93aa9737bfcd30c149
jafarRezaei <bomber.man87@yahoo.com>
2020-06-08 12:31:15 +0430

    fix tests and exports

--- packages/icons/packages/Followup/index.js
+++ packages/icons/packages/Followup/index.js
@@ -1,4 +1,8 @@
+export { default as BoxingIcon } from './BoxingIcon/component';
 export { default as BoxWithTickIcon } from './BoxWithTickIcon/component';
+export { default as FruitBasketIcon } from './FruitBasketIcon/component';
 export { default as GroceryBagIcon } from './GroceryBagIcon/component';
 export { default as ListSearchIcon } from './ListSearchIcon/component';
 export { default as MotorcycleIcon } from './MotorcycleIcon/component';
+export { default as SaleIcon } from './SaleIcon/component';
+export { default as TruckIcon } from './TruckIcon/component';
--- packages/icons/packages/Followup/sprite.js
+++ packages/icons/packages/Followup/sprite.js
@@ -1,4 +1,8 @@
+export { default as BoxingIcon } from './BoxingIcon/sprite';
 export { default as BoxWithTickIcon } from './BoxWithTickIcon/sprite';
+export { default as FruitBasketIcon } from './FruitBasketIcon/sprite';
 export { default as GroceryBagIcon } from './GroceryBagIcon/sprite';
 export { default as ListSearchIcon } from './ListSearchIcon/sprite';
 export { default as MotorcycleIcon } from './MotorcycleIcon/sprite';
+export { default as SaleIcon } from './SaleIcon/sprite';
+export { default as TruckIcon } from './TruckIcon/sprite';
