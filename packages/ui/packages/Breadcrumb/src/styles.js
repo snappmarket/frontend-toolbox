1220038d2b3bbf301de173973d2f1c4a61fcb449
jafarRezaei <bomber.man87@yahoo.com>
2020-06-08 13:12:31 +0430

    fix breadcrumb semantic HTML

--- packages/ui/packages/Breadcrumb/src/styles.js
+++ packages/ui/packages/Breadcrumb/src/styles.js
@@ -1,35 +1,32 @@
 // eslint-disable-next-line import/no-named-default
 import { default as Styled } from 'styled-components';
-const StyledBreadcrumbWrapper = Styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: start;
-    align-items: center;
-    background-color:  ${props => props.theme.colors.white};
-    border-radius: calc(${props => props.theme.defaultRem} * 0.2);
-    font-size: calc(${props => props.theme.defaultRem} * 1.5);
-`;
-const StyledBreadcrumbItem = Styled.div`
-    padding: calc(${props => props.theme.defaultRem});
-    a {
-        text-decoration: none;
-        color: ${props => props.theme.colors.gray.normal};
-        font-size: calc(${props => props.theme.defaultRem} * 1.2);
-    }
-    h2 {
-        color: ${props => props.theme.colors.gray.normal};
-        font-size: calc(${props => props.theme.defaultRem} * 1.2);
-    }
-`;
-const StyledBreadcrumbItemSeparator = Styled.div``;
-const StyledBreadcrumbItemWrapper = Styled.div`
-    display: flex;
-    align-items: center;
+
+export const StyledBreadcrumbWrapper = Styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  background-color:  ${props => props.theme.colors.white};
+  border-radius: calc(${props => props.theme.defaultRem} * 0.2);
+  font-size: calc(${props => props.theme.defaultRem} * 1.5);
 `;
 
-export {
-  StyledBreadcrumbWrapper,
-  StyledBreadcrumbItem,
-  StyledBreadcrumbItemSeparator,
-  StyledBreadcrumbItemWrapper,
-};
+export const StyledBreadcrumbItem = Styled.div`
+  padding: calc(${props => props.theme.defaultRem});
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.colors.gray.normal};
+    font-size: calc(${props => props.theme.defaultRem} * 1.2);
+  }
+  h2 {
+    color: ${props => props.theme.colors.gray.normal};
+    font-size: calc(${props => props.theme.defaultRem} * 1.2);
+  }
+`;
+
+export const StyledBreadcrumbItemSeparator = Styled.div``;
+
+export const StyledBreadcrumbItemWrapper = Styled.div`
+  display: flex;
+  align-items: center;
+`;
