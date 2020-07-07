6502b8396298a8e19948c323cab94d7b677f55bc
jafarRezaei <bomber.man87@yahoo.com>
2020-07-07 11:38:01 +0430

    add new icons

---
+++ packages/icons/packages/ExtraRegularIcons/TrashBoldIcon/component/index.js
@@ -0,0 +1,55 @@
+/**
+ * THIS IS AN AUTO GENERATED FILE, CHANGES WILL NOT APPLY
+ */
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+const TrashBoldIcon = ({ className, size }) => (
+  <svg
+    data-testid="TrashBoldIcon"
+    viewBox="0 0 9 11"
+    className={className}
+    style={{
+      width: size * 10,
+      height: size * 10,
+    }}
+    focusable="false"
+    fill="currentColor"
+  >
+    <title>trash-alt</title>
+    <g
+      stroke="none"
+      strokeWidth="1"
+      fill="none"
+      fillRule="evenodd"
+      font-family="FontAwesome5Free-Regular, Font Awesome 5 Free"
+      font-size="10"
+      font-weight="normal"
+    >
+      <g transform="translate(-792.000000, -94.000000)" fill="#757575">
+        <g transform="translate(749.000000, 13.000000)">
+          <g transform="translate(24.000000, 24.000000)">
+            <g transform="translate(0.000000, 44.000000)">
+              <text>
+                <tspan x="19.25" y="22">
+                  trash-alt
+                </tspan>
+              </text>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+TrashBoldIcon.propTypes = {
+  className: PropTypes.string,
+  size: PropTypes.number,
+};
+
+TrashBoldIcon.defaultProps = {
+  size: 1.5,
+};
+
+export default TrashBoldIcon;
