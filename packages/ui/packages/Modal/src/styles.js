1db777afb0ad3772a7e911aaf60a19cd8ce7c29a
mahsa <mahsa_mesbah@yahoo.com>
2020-07-19 14:21:06 +0430

    add animation class for open and close modal

--- packages/ui/packages/Modal/src/styles.js
+++ packages/ui/packages/Modal/src/styles.js
@@ -31,6 +31,25 @@ const StyledModal = Styled.div`
   padding: calc(${props => props.theme.defaultRem});
   border-radius: calc(${props => props.theme.defaultRem} * 0.5);
   width: calc(${props => props.theme.defaultRem} * ${props => props.width});
+
+  .animate-visibility {
+    will-change: transform, opacity, max-height, visibility;
+    max-height: 0;
+    opacity: 0;
+    overflow: hidden;
+    visibility: collapse;
+    transform: translateZ(0) translateY(calc(${props.theme.defaultRem} * -1)) scale(0.9);
+    transition: transform 0.2s ease-out, opacity 0.2s ease-out, max-height 0.2s ease-out, visibility 0.2s ease-out;
+  
+    &.visible {
+      opacity: 1;
+      max-height: 100%;
+      visibility: visible;
+      transform: translateZ(0) translateY(0) scale(1);
+      transition: transform 0.3s ease-in, opacity 0.3s ease-in, max-height 0.3s ease-in, visibility 0.3s ease-in;
+      overflow: auto !important;
+    }
+  };
   ${props =>
     props.position === 'top' &&
     `
