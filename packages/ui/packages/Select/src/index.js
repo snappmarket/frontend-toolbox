a6fb05616171bed9f5f3ac695e140efa78cbd1b4
mostafa rastegar <m.rastegar991@gmail.com>
2020-07-29 19:44:41 +0430

    Add select on basic mode

---
+++ packages/ui/packages/Select/src/index.js
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { AngleDownIcon } from '@snappmarket/icons/sprite';
+import {
+  StyledSelectWrapper,
+  StyledSelectIconWrapper,
+  StyledSelect,
+  StyledLabel,
+  StyledStar,
+  StyledMessage,
+} from './styles';
+
+const Select = React.forwardRef((props, ref) => {
+  const {
+    status,
+    message,
+    label,
+    required,
+    className,
+    options,
+    ...rest
+  } = props;
+  return (
+    <StyledSelectWrapper data-testid="inputWrapper">
+      {label && (
+        <StyledLabel data-testid="inputLabel" htmlFor={rest.id || ''}>
+          {label}{' '}
+          {required && (
+            <StyledStar data-testid="labelRequiredFlag">*</StyledStar>
+          )}
+        </StyledLabel>
+      )}
+      <StyledSelectIconWrapper>
+        <AngleDownIcon />
+        <StyledSelect
+          name={label}
+          ref={ref}
+          className={className}
+          status={status}
+          {...rest}
+        >
+          {options.map(option => (
+            <option key={option.key} value={option.value}>
+              {option.title}
+            </option>
+          ))}
+        </StyledSelect>
+      </StyledSelectIconWrapper>
+      {Object.keys(message).length > 0 && (
+        <StyledMessage data-testid="inputMessages" type={message.type}>
+          {message.content}
+        </StyledMessage>
+      )}
+    </StyledSelectWrapper>
+  );
+});
+
+Select.propTypes = {
+  label: PropTypes.string,
+  status: PropTypes.oneOf(['gray', 'red', 'blue', 'orange', 'green', 'yellow']),
+  value: PropTypes.any,
+  placeholder: PropTypes.any,
+  message: PropTypes.shape({
+    type: PropTypes.oneOf(['success', 'danger']),
+    content: PropTypes.string,
+  }),
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+  required: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  options: PropTypes.array,
+};
+
+Select.defaultProps = {
+  label: '',
+  status: 'gray',
+  required: false,
+  message: {},
+};
+
+export default Select;
---
+++ packages/ui/packages/Select/src/styles.js
@@ -0,0 +1,70 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line import/no-named-default
+import { default as Styled } from 'styled-components';
+const StyledSelectWrapper = Styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  width: 100%;
+  margin: calc(${props => props.theme.defaultRem} * 0.8) auto;
+`;
+
+const StyledSelectIconWrapper = Styled.div`
+  position: relative;
+  svg {
+    position: absolute;
+    bottom: calc(${props => props.theme.defaultRem} * 1.3);
+    left: calc(${props => props.theme.defaultRem} * 1);
+    width: calc(${props => props.theme.defaultRem} * 1) !important;
+    fill: ${props => props.theme.colors.gray.dark};
+  }
+`;
+
+const StyledSelect = Styled.select`
+  width: 100%;
+  border: calc(${props => props.theme.defaultRem} * 0.1) solid
+    ${props => props.theme.colors[props.status]['ultra-light']};
+  font-size: calc(${props => props.theme.defaultRem} * 1.4);
+  height: calc(${props => props.theme.defaultRem} * 4);
+  width: 100%;
+  padding: 0 calc(${props => props.theme.defaultRem} * 1.5);
+  border-radius: calc(${props => props.theme.defaultRem} * 0.3);
+  transition: all 0.3s ease 0s;
+  box-shadow: none;
+  outline: none;
+  -webkit-appearance: none;
+  &:hover {
+    border-color: ${props => props.theme.colors[props.status].light};
+  }
+  &:disabled {
+  }
+  &::placeholder {
+    color: ${props => props.theme.colors.gray.normal};
+  }
+`;
+
+const StyledLabel = Styled.label`
+  color: ${props => props.theme.colors.gray.dark};
+  font-size: calc(${props => props.theme.defaultRem} * 1.4);
+  margin-bottom: calc(${props => props.theme.defaultRem} * 0.6);
+`;
+
+const StyledStar = Styled.span`
+  color: ${props => props.theme.colors.red.normal};
+  margin-right: calc(${props => props.theme.defaultRem} * 0.2);
+`;
+
+const StyledMessage = Styled.div`
+  color: ${props =>
+    props.theme.colors[props.type === 'success' ? 'green' : 'red'].normal};
+  font-size: calc(${props => props.theme.defaultRem} * 1.2);
+  margin-top: calc(${props => props.theme.defaultRem} * 0.8);
+`;
+
+export {
+  StyledSelectWrapper,
+  StyledSelectIconWrapper,
+  StyledSelect,
+  StyledLabel,
+  StyledStar,
+  StyledMessage,
+};
