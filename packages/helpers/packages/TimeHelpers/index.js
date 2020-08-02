d302e1d3a799f675d73d3073b7108beb191cbed8
mahsa <mahsa_mesbah@yahoo.com>
2020-08-02 13:27:52 +0430

    add timeHelpers in helpers

---
+++ packages/helpers/packages/TimeHelpers/index.js
@@ -0,0 +1,70 @@
+import { delog } from '@snappmarket/helpers';
+/**
+ * @function
+ * @name getTimeWithMilliSeconds
+ * @description get time with milli seconds
+ * @returns  {object}
+ */
+export const getTimeWithMilliSeconds = () => {
+  const localTime = new Date().toLocaleTimeString();
+  return `${localTime.split(' ')[0]}:${new Date().getMilliseconds()}`;
+};
+
+/**
+ * @function
+ * @name getClock
+ * @description get date minute and seconds 
+ * @param     dateString       {string}    given dateString to convert Date format with minutes and seconds 
+ * @returns   {object}
+ */
+export const getClock = dateString => {
+  const date = new Date(dateString);
+  const minute = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return {
+    hour: date.getHours(),
+    minute,
+    seconds,
+    fullMinute: minute.toString().length === 1 ? `0${minute}` : minute,
+    fullSeconds: seconds.toString().length === 1 ? `0${seconds}` : seconds,
+  };
+};
+
+/**
+ * @function
+ * @name getDiffInDays
+ * @description get two date and calculate different of them
+ * @param   date1    {string}
+ * @param   date2    {string} 
+ * @returns {object}
+ */
+export const getDiffInDays = (date1, date2 = new Date()) => {
+  const diffTime = Math.abs(date1 - date2);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return {
+    diffDays,
+    diffTime,
+  };
+};
+
+/**
+ * @function
+ * @name timeDiff
+ * @description different time
+ * @param     title       {string} 
+ */
+const times = {};
+export class timeDiff {
+  static start(title) {
+    times[title] = Date.now();
+  }
+
+  static show(title) {
+    delog(
+      `🕑 ${title} took: ${(Date.now() - times[title]).toLocaleString(
+        'es-US',
+      )}ms`,
+    );
+  }
+}
