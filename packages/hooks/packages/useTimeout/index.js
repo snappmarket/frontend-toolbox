febe7ce464481ce10d848433781908dfdbbaa419
jafarRezaei <bomber.man87@yahoo.com>
2020-08-03 11:26:54 +0430

    add useTimeout hook

---
+++ packages/hooks/packages/useTimeout/index.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useCallback } from 'react';
+
+const useTimeout = (callback, delay) => {
+  // save id in a ref
+  const timeoutId = useRef('');
+
+  // save callback as a ref so we can update the timeout callback without resetting the clock
+  const savedCallback = useRef();
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // clear the timeout and start a new one, updating the timeoutId ref
+  const reset = useCallback(() => {
+    clearTimeout(timeoutId.current);
+
+    const id = setTimeout(savedCallback.current, delay);
+    timeoutId.current = id;
+  }, [delay]);
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (delay !== null) {
+      reset();
+
+      return () => clearTimeout(timeoutId.current);
+    }
+  }, [delay, reset]);
+
+  return { reset };
+};
+
+export default useTimeout;
