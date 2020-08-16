34da10a44bd40ed534a5501ea7015e4af62224d9
Reza Erami <reza@erami.name>
2020-08-16 15:30:29 +0430

    use theme helpers in option group

--- packages/ui/packages/OptionGroup/src/styles.js
+++ packages/ui/packages/OptionGroup/src/styles.js
@@ -1,36 +1,36 @@
 // eslint-disable-next-line import/no-named-default
 import { default as Styled } from 'styled-components';
-const StyledOptionGroup = Styled.div`
+import {rem, color} from '@snappmarket/helpers'
+
+export const StyledOptionGroup = Styled.div`
   width: 100%;
 `;
 
-const StyledOptionItem = Styled.div`
+export const StyledOptionItem = Styled.div`
   display: flex;
   width: 100%;
   align-items: center;
   opacity: ${props => (props.disabled ? '0.5' : '1')};
   cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
   &:not(:first-child) {
-    margin-top:calc(${props => props.theme.defaultRem});
+    margin-top: ${rem(1)};
   }
   > span {
     width: 100%;
   }
   button {
     svg {
-      fill: ${props => props.theme.colors.white};
+      fill: ${color('white')};
     }
     &:active {
       svg {
-        fill: ${props => props.theme.colors.white};
+        fill: ${color('white')};
       }
     }
   }
   button > svg {
     margin-left: 0 !important;
-    width: calc(${props => props.theme.defaultRem} * 1.5) !important;
-    height: calc(${props => props.theme.defaultRem} * 1.5) !important;
+    width: ${rem(1.5)} !important;
+    height: ${rem(1.5)} !important;
   }
 `;
-
-export { StyledOptionGroup, StyledOptionItem };
