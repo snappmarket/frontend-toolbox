775c199c999845e09c6d597032d54bede19f2f84
Reza Erami <reza@erami.name>
2020-08-16 15:33:24 +0430

    use theme helpers in pagination

--- packages/ui/packages/Pagination/src/styles.js
+++ packages/ui/packages/Pagination/src/styles.js
@@ -1,48 +1,43 @@
 // eslint-disable-next-line import/no-named-default
 import { default as Styled } from 'styled-components';
-const StyledPaginationWrapper = Styled.nav``;
-const StyledPaginationItemsWrapper = Styled.ul`
+import {rem, color} from '@snappmarket/helpers'
+
+export const StyledPaginationWrapper = Styled.nav``;
+export const StyledPaginationItemsWrapper = Styled.ul`
   list-style: none;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0 calc(${props => props.theme.defaultRem} * -0.4);
+  margin: ${rem(0, -0.4)};
   cursor: pointer;
 `;
-const StyledNavigateButton = Styled.li`
-  border-radius: calc(${props => props.theme.defaultRem} * 0.5);
-  color: ${props => props.theme.colors.taupe};
-  width: calc(${props => props.theme.defaultRem} * 4);
-  height: calc(${props => props.theme.defaultRem} * 4);
-  font-size: calc(${props => props.theme.defaultRem} * 1.6);
-  margin: 0 calc(${props => props.theme.defaultRem} * 0.4);
+export const StyledNavigateButton = Styled.li`
+  border-radius: ${rem(0.5)};
+  color: ${color('taupe')};
+  width: ${rem(4)};
+  height: ${rem(4)};
+  font-size: ${rem(1.6)};
+  margin: ${rem(0, 0.4)};
   display: flex;
   align-items: center;
   justify-content: center;
   svg {
     &:first-child {
-      margin-left: calc(${props => props.theme.defaultRem} * -0.8)
+      margin-left: ${rem(-0.8)};
     }
   }
   &[disabled] {
     opacity: 0.7;
   }
 `;
-const StyledPaginationItem = Styled(StyledNavigateButton)`
-  background-color: ${props => props.theme.colors.blue.bright};
+export const StyledPaginationItem = Styled(StyledNavigateButton)`
+  background-color: ${color('blue', 'bright')};
   transition: all 0.3s ease 0.1s;
   &:hover, &.active {
-    background-color: ${props => props.theme.colors.blue.normal};
-    color: ${props => props.theme.colors.white};
+    background-color: ${color('blue', 'normal')};
+    color: ${color('white')};
   }
   &.active:hover {
     cursor: default;
   }
 `;
-
-export {
-  StyledPaginationWrapper,
-  StyledPaginationItemsWrapper,
-  StyledNavigateButton,
-  StyledPaginationItem,
-};
