9af5bba1cc94b22ab535be4f20545440f532930c
jafarRezaei <bomber.man87@yahoo.com>
2020-09-06 15:45:12 +0430

    fix docz and eslint

--- doczrc.js
+++ doczrc.js
@@ -3,5 +3,5 @@ export default {
   title: 'Snappmarket frontend toolbox',
   files: '**/*.{markdown,mdx}',
   port: '9002',
-  ignore: ['readme.md', 'changelog.md', 'code_of_conduct.md', 'contributing.md', 'license.md'],
+  ignore: ['readme.md', 'changelog.md', 'code_of_conduct.md', 'contributing.md', 'license.md', '.idea'],
 };
