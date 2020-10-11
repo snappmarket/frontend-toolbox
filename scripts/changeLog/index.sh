50d7ee080be668ac4e71e69eff165d91b368788b
jafarRezaei <bomber.man87@yahoo.com>
2020-10-11 11:12:51 +0330

    fix release:note script

--- scripts/changeLog/index.sh
+++ scripts/changeLog/index.sh
@@ -11,14 +11,13 @@
 NOW=$(date +'%Y-%m-%d %H:%M:%S')
 CURRENT_GIT_VERSION=$(git describe --tags --abbrev=0 || '1.0.0')
 NEW_GIT_VERSION=$(bash ./utils/semver bump patch $CURRENT_GIT_VERSION)
-NEW_TAG_CHANGES=$(git log --no-merges --pretty=format:"- %s (**%cn** - %cr)" HEAD...0.3.14)
+NEW_TAG_CHANGES=$(git log --no-merges --pretty=format:"- %s (**%cn** - %cr)" HEAD...$NEW_GIT_VERSION)
 
 # Find task ids and link to them
 re='(.*)#(0-9)+(.*)'
-while [[ $hello =~ $re ]]; do
+while [[ $NEW_TAG_CHANGES =~ $re ]]; do
   NEW_TAG_CHANGES=${BASH_REMATCH[1]} + "[#${BASH_REMATCH[2]}](https://jira.snapp.market/browse/SP-${BASH_REMATCH[2]})" + ${BASH_REMATCH[3]}
 done
-echo "$hello"
 
 echo "## $NEW_GIT_VERSION" > changeslog.md
 echo $NOW >> changeslog.md
@@ -28,6 +27,6 @@ echo "" >> changeslog.md
 echo "" >> changeslog.md
 cat ../../changeslog.md >> changeslog.md
 mv changeslog.md ../../changeslog.md
-git tag -a $NEW_GIT_VERSION -m "$NEW_TAG_CHANGES"
+#git tag -a $NEW_GIT_VERSION -m "$NEW_TAG_CHANGES"
 
 echo "🚀 version $NEW_GIT_VERSION pushed successfully"
