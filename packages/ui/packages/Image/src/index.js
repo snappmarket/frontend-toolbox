20b9a78fbc8e82586583e116e7af7b3342205caa
jafarRezaei <bomber.man87@yahoo.com>
2020-10-23 09:27:01 +0330

    use new version of react-image

--- packages/ui/packages/Image/src/index.js
+++ packages/ui/packages/Image/src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import Img from 'react-image';
+import { Img } from 'react-image';
 
 import { LogoAnimation } from '../../Loading/src';
 
