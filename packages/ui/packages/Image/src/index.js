9684427027b1fd93be0e9c0b506f9a2e7251b203
Jafa Rezaei <sayjeyhi@gmail.com>
2020-11-23 10:54:50 +0330

    add webP support to Image component

--- packages/ui/packages/Image/src/index.js
+++ packages/ui/packages/Image/src/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { Img } from 'react-image';
+import { useImage } from 'react-image';
 
 import { LogoAnimation } from '../../Loading/src';
 
@@ -10,18 +11,92 @@ import { LogoAnimation } from '../../Loading/src';
  * @returns {*}
  * @constructor
  */
-const Image = ({ src, size, loader, error, ...rest }) => (
-  <Img
-    data-testid="image"
-    src={src}
-    loader={loader || <LogoAnimation size={size} />}
-    unloader={error || <LogoAnimation size={size} animate={false} />}
-    {...rest}
-  />
-);
+const Image = ({ src, size, loader, error, ...rest }) => {
+  /**
+   * Ensure we have webp support
+   * @type {boolean}
+   */
+  const hasWebPSupport = useMemo(() => {
+    const elem = document.createElement('canvas');
+    if (elem.getContext && elem.getContext('2d')) {
+      return elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+    }
+    return false;
+  }, []);
+
+  /**
+   * Preload supported image
+   * @type {arg is Array<any>}
+   */
+  const hasMultiSrc = Array.isArray(src);
+  let webpAddress = '';
+  let imageAddress = src;
+  if (hasMultiSrc) {
+    webpAddress = (
+      src.find(s => s.address.split('.').pop() === 'webp') || {
+        address: '',
+      }
+    ).address;
+    imageAddress = (
+      src.find(s => s.address.split('.').pop() !== 'webp') || {
+        address: '',
+      }
+    ).address;
+  }
+  const { isLoading, error: hasError } = useImage({
+    srcList: hasWebPSupport && webpAddress ? webpAddress : imageAddress,
+    useSuspense: false,
+  });
+
+  /**
+   * Render picture tag
+   * @param image
+   * @returns {*}
+   */
+  const renderPicture = image => {
+    const address = image.address || image;
+    const type = address.split('.').pop();
+    const sourceTypes = [
+      { extension: 'avif', type: 'image/avif' },
+      { extension: 'webp', type: 'image/webp' },
+    ];
+    const isSource = sourceTypes.filter(t => t.extension === type).length;
+    const props = type.props || {};
+
+    return !isSource ? (
+      <img src={address} alt={props.alt || ''} {...rest} />
+    ) : (
+      <source srcSet={address} {...props} />
+    );
+  };
+
+  const Loader = loader || <LogoAnimation size={size} />;
+  const Error = error || <LogoAnimation size={size} animate={false} />;
+
+  // eslint-disable-next-line no-nested-ternary
+  return isLoading ? (
+    Loader
+  ) : hasError ? (
+    Error
+  ) : (
+    <picture data-testid="image">
+      {Array.isArray(src)
+        ? src.map(image => renderPicture(image))
+        : renderPicture(src)}
+    </picture>
+  );
+};
 
 Image.propTypes = {
-  src: PropTypes.string.isRequired,
+  src: PropTypes.oneOf([
+    PropTypes.string.isRequired,
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        address: PropTypes.string,
+        props: PropTypes.object,
+      }),
+    ),
+  ]),
   loader: PropTypes.oneOfType([PropTypes.node, PropTypes.bool]),
   error: PropTypes.oneOfType([PropTypes.node, PropTypes.bool]),
   size: PropTypes.number,
